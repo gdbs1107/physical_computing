60e0a3defa992babd0ab72aad9c18cf27229d7f7
gdbs1107 <gdbs1107@naver.com>
2025-06-05 22:19:01 +0900

    init: 프로젝트 초기설정

---
+++ src/test/java/or/umc/physical/PhysicalApplicationTests.java
@@ -0,0 +1,13 @@
+package or.umc.physical;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhysicalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
