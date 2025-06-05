169e3f148a55ab9e496b589b20e8b948e761daee
gdbs1107 <gdbs1107@naver.com>
2025-06-05 22:36:54 +0900

    feat: 주연이 메일 보내기 스켈레톤 구현

--- src/main/java/or/umc/physical/JuYeonService.java
+++ src/main/java/or/umc/physical/JuYeonService.java
@@ -15,10 +15,18 @@ public class JuYeonService {
     @Transactional
     public void wakeUp(){
         JuYeon juyeon = juYeonRepository.findById(1L)
-                .orElseThrow(() -> new RuntimeException("JuYeon not found"));
+                .orElseThrow(() -> new RuntimeException("주연이를 찾을 수가 없어요"));
 
         juyeon.addAlarmCount();
         juYeonRepository.save(juyeon);
+
+        if((juyeon.getAlarmCount() % 5) ==0){
+            wakeUpMail();
+        }
+    }
+
+    private void wakeUpMail(){
+        throw new IllegalArgumentException("주연이한테 일어나라고 메일 보내기");
     }
 
 }
