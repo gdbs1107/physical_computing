6bbecc81b7f5c59ab10ecd3af66aa1644f1fb285
gdbs1107 <gdbs1107@naver.com>
2025-06-06 00:06:54 +0900

    feat: 주연이 메일 환경변수 추가

--- src/main/java/or/umc/physical/mail/MailUtil.java
+++ src/main/java/or/umc/physical/mail/MailUtil.java
@@ -23,6 +23,9 @@ public class MailUtil {
     @Value("${spring.mail.username}")
     private String configEmail;
 
+    @Value("${juyeon.mail}")
+    private String juyeonMail;
+
     public void sendEmail() throws MessagingException {
 
         MimeMessage emailForm = createEmailForm();
@@ -53,8 +56,7 @@ public class MailUtil {
     private MimeMessage createEmailForm() throws MessagingException {
         MimeMessage message = mailSender.createMimeMessage();
 
-        /*message.addRecipients(MimeMessage.RecipientType.TO, "jeonjy0630@naver.com");*/
-        message.addRecipients(MimeMessage.RecipientType.TO, "gdbs1107@naver.com");
+        message.addRecipients(MimeMessage.RecipientType.TO, juyeonMail);
         message.setSubject("주연아 일어나라 X100 🔔");
         message.setFrom(configEmail);
 
